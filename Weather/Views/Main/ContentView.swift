88c3a0b0ef60e6a5a56c5b84caaac0f4701ddb52
Aimeric Tshibuaya <tshibuayaaimeric@gmail.com>
2022-07-09 12:24:06 -0400

    WeatherApp UI Design

---
+++ Weather/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Aimeric on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aimeric on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
